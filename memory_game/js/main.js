0765770ce5aa05a20cda7796c477e6cea1956744
Femi Coker <femicoker@Femis-MacBook-Pro.local>
2018-08-30 18:44:23 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,15 @@
-console.log("up and running");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay =[];
+var cardOne = cards[0];
+var cardTwo = cards[3];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("user flipped" + " " + cardOne);
 console.log("user flipped" + " " + cardTwo);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+	alert("you found a match");
+} else {
+	alert("sorry, try again!");
+}
