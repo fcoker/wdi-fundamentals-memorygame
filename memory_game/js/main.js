396f0583712e608533c82ed768efe32affac3016
Femi Coker <femicoker@Femis-MacBook-Pro.local>
2018-08-31 11:14:20 +0100

    created game logic - js  code organisation

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,19 @@
 var cards = ["queen", "queen", "king", "king"]
 var cardsInPlay =[];
-var cardOne = cards[0];
-var cardTwo = cards[3];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped" + " " + cardOne);
-console.log("user flipped" + " " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+var checkForMatch = function(){
+   if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
 	alert("you found a match");
-} else {
+    } else {
 	alert("sorry, try again!");
+    }
 }
+
+var flipCard = function(cardId){
+	//i dont understand how "cards[cardId]" works!?
+console.log("user flipped" + " " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
