4f0ae6a3651c89dc60acda90d84d1fcb34262919
Femi Coker <femicoker@femis-mbp.home>
2018-09-02 08:55:23 +0100

    still figuring out final piece of code

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,20 +23,46 @@ var cards = [
 
 var cardsInPlay =[];
 var checkForMatch = function(){
-   if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+	this.setAttribute('src', cards[cardId].cardImage);
+    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
 	alert("you found a match");
     } else {
 	alert("sorry, try again!");
     }
 }
 
-var flipCard = function(cardId){
-cardsInPlay.push(cards[cardId].rank);
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 console.log("user flipped" + " " + cards[cardId].rank);
-checkForMatch();
+cardsInPlay.push(cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
+
+this.setAttribute('src', cards[cardId].cardImage);
+
+
+checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+
+var createBoard = function(){
+	for (i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+       
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+}
+
+createBoard();
+
+
+
+
+
+
+
+
